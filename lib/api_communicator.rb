65ff1821f3419d9e47535ddb9c09a1207495e52a
Marco-FI-SEI <mmc90@hotmail.co.uk>
2019-09-17 15:31:05 +0100

    Add solution - Marco

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,60 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
+# get a Ruby hash from API call
+def get_response
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+# get character info
+def get_all_character_info(response)
+  characters = response["results"]
+end
+
+# return film urls for specified character
+def get_character_film_urls(all_character_info, character_name)
+  all_character_info.each do |character_info|
+    name = character_info.first[1].downcase
+    if name == character_name
+      films = character_info["films"]
+      return films
+    end
+  end
+end
+
+# Query the API to get film instances
+def get_film_info(films)
+  film_objects = []
+  films.each do |film|
+    film_string = RestClient.get(film)
+    film_hash = JSON.parse(film_string)
+    film_objects.push(film_hash)
+  end
+  return film_objects
+end
+
+# retrieve film titles from film info
+def get_film_titles(films_info)
+  film_titles = []
+  films_info.each do |info|
+    film_titles.push(info.first[1])
+  end
+  return film_titles
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |title|
+    puts title
+  end
 end
 
+# Call above methods
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+  response = get_response
+  all_character_info = get_all_character_info(response)
+  character_films_urls = get_character_film_urls(all_character_info, character)
+  film_info = get_film_info(character_films_urls)
+  film_titles = get_film_titles(film_info)
+  print_movies(film_titles)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
